b12dab88d5d83486805fa9b6964dfce225308db4
Areejslama <Areegahmed449@gmail.com>
2024-03-10 14:15:20 -0700

    style

--- models/city.py
+++ models/city.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """represent a class"""
+
 from models.base_model import BaseModel
+
 
 class City(BaseModel):
     state_id = ''
--- models/state.py
+++ models/state.py
@@ -2,6 +2,7 @@
 """represent a state class"""
 from models.base_model import BaseModel
 
+
 class State(BaseModel):
     """define a class"""
     name = ''
