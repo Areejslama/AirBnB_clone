5736cdce4b31cf8ee067cc3e147a877de08ede85
Areejslama <Areegahmed449@gmail.com>
2024-03-10 12:08:03 -0700

    my

--- console.py
+++ console.py
@@ -135,11 +135,10 @@ class HBNBCommand(cmd.Cmd):
         objs = models.storage.all()
 
         if an_instance:
-            keys = objs.keys()
-            return [str(val) for key, val in objs.items()
-                    if key.startswith(an_instance)]
+            return [str(val) for key, val in objs.items() if key.startswith(an_instance)]
 
-        return [str(val) for key, val in objs.items()]
+        return [str(val) for val in objs.values()]
+
 
     def default(self, line):
         """handle entered commands"""
